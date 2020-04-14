d0e9b127f6baf13df90e1d1d9fba151a8e522e5a
Álejandro Moracen <amoracen93@gmail.com>
2020-04-14 19:22:51 -0400

    feat: allow user to login using email and password

--- app/src/main/java/fau/amoracen/covid_19update/ui/mainActivity/LoginFragment.java
+++ app/src/main/java/fau/amoracen/covid_19update/ui/mainActivity/LoginFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,20 +13,27 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import java.util.HashMap;
 import java.util.Objects;
 
+import fau.amoracen.covid_19update.service.FirebaseUtil;
 import fau.amoracen.covid_19update.ui.homeActivity.HomeActivity;
 import fau.amoracen.covid_19update.R;
 
-public class LoginFragment extends Fragment {
+/**
+ * Manages Login Form
+ */
+public class LoginFragment extends Fragment implements FirebaseUtil.FirebaseUtilListener {
 
     private EditText emailEditText, passwordEditText;
+    private String email, password;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -59,24 +67,8 @@ public class LoginFragment extends Fragment {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-
-                if (email.isEmpty()) {
-                    /*TODO VALIDATE EMAIL*/
-                    emailEditText.setError("Please enter your email address");
-                    emailEditText.setText("");
-                    emailEditText.requestFocus();
-                } else if (password.isEmpty()) {
-                    passwordEditText.setError("Please enter your password");
-                    passwordEditText.setText("");
-                    passwordEditText.requestFocus();
-                } else {
-                    /*TODO FIREBASE*/
-                    //Go to Dashboard
-                    Intent goToDashboard = new Intent(getContext(), HomeActivity.class);
-                    startActivity(goToDashboard);
-                    Objects.requireNonNull(getActivity()).finishAffinity();
+                if (checkEmailAndPassword()) {
+                    signIn();
                 }
             }
         });
@@ -88,5 +80,55 @@ public class LoginFragment extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_RegistrationPassword, null);
             }
         });
+    }
+
+    /**
+     * Connect with Firebase to sign in
+     */
+    private void signIn() {
+        FirebaseUtil.getInstance().signInWithEmailAndPassword(email, password, this);
+    }
+
+    /**
+     * Validate Email and Password
+     *
+     * @return true if both are valid
+     */
+    private boolean checkEmailAndPassword() {
+        email = emailEditText.getText().toString();
+
+        HashMap hmap = FirebaseUtil.getInstance().validateEmail(getContext(), email);
+        if (!Objects.equals(hmap.get("message"), "Valid")) {
+            emailEditText.setError(Objects.requireNonNull(hmap.get("message")).toString());
+            emailEditText.setText("");
+            emailEditText.requestFocus();
+            return false;
+        }
+        password = passwordEditText.getText().toString();
+        hmap = FirebaseUtil.getInstance().validatePassword(getContext(), password);
+        if (!Objects.equals(hmap.get("message"), "Valid")) {
+            passwordEditText.setError(Objects.requireNonNull(hmap.get("message")).toString());
+            passwordEditText.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Get result from Firebase sign in listener
+     *
+     * @param result a string
+     */
+    @Override
+    public void onCompleteSendResult(String result) {
+        if (result.contains("Success")) {
+            /*Go to Home Screen*/
+            Intent goToHomeScreen = new Intent(getContext(), HomeActivity.class);
+            startActivity(goToHomeScreen);
+            Objects.requireNonNull(getActivity()).finishAffinity();
+        } else {
+            Log.i("Failed Login", result);
+            Toast.makeText(getContext(), result, Toast.LENGTH_LONG).show();
+        }
     }
 }
