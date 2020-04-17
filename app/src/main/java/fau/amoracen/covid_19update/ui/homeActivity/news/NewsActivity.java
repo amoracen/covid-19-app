7fb25162d7be89d04b78c48d9cf393d255957b77
Álejandro Moracen <amoracen93@gmail.com>
2020-04-17 01:25:59 -0400

    Initial Commit

---
+++ app/src/main/java/fau/amoracen/covid_19update/ui/homeActivity/news/NewsActivity.java
@@ -0,0 +1,54 @@
+package fau.amoracen.covid_19update.ui.homeActivity.news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.Objects;
+
+import fau.amoracen.covid_19update.R;
+import fau.amoracen.covid_19update.data.CountryData;
+import fau.amoracen.covid_19update.data.NewsArticles;
+
+public class NewsActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private NewsArticles.Articles article;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+        /*Get Data*/
+        Intent intent = getIntent();
+        article = (NewsArticles.Articles) Objects.requireNonNull(intent.getExtras()).getSerializable("News");
+        if (article == null) finish();
+        webView = findViewById(R.id.webView);
+        webView.setInitialScale(1);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        webView.loadUrl(article.getUrl());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
