1478406e92bd269c72ad607810c2ec82607f66c5
Álejandro Moracen <amoracen93@gmail.com>
2020-04-17 00:58:26 -0400

    Initial Commit

---
+++ app/src/main/java/fau/amoracen/covid_19update/data/NewsArticles.java
@@ -0,0 +1,138 @@
+package fau.amoracen.covid_19update.data;
+
+import java.io.Serializable;
+
+public class NewsArticles implements Serializable {
+
+    String status;
+    String totalResults;
+    Articles[] articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Articles[] getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Articles[] articles) {
+        this.articles = articles;
+    }
+
+
+    /**
+     * Class Articles
+     */
+    public static class Articles implements Serializable {
+        Source source;
+
+        public Source getSource() {
+            return source;
+        }
+
+        public void setSource(Source source) {
+            this.source = source;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+
+        public String getUrlToImage() {
+            return urlToImage;
+        }
+
+        public void setUrlToImage(String urlToImage) {
+            this.urlToImage = urlToImage;
+        }
+
+        public String getPublishedAt() {
+            return publishedAt;
+        }
+
+        public void setPublishedAt(String publishedAt) {
+            this.publishedAt = publishedAt;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public static class Source implements Serializable {
+            String id;
+            String name;
+
+            public String getId() {
+                return id;
+            }
+
+            public void setId(String id) {
+                this.id = id;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+
+        String author;
+        String title;
+        String description;
+        String url;
+        String urlToImage;
+        String publishedAt;
+        String content;
+
+    }//EOF Class Articles
+
+}
