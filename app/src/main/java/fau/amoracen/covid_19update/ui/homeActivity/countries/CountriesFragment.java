5211361de16ebd655fc3a00a8214ecae0d3e7b16
Álejandro Moracen <amoracen93@gmail.com>
2020-04-24 18:00:12 -0400

    rename search menu

--- app/src/main/java/fau/amoracen/covid_19update/ui/homeActivity/countries/CountriesFragment.java
+++ app/src/main/java/fau/amoracen/covid_19update/ui/homeActivity/countries/CountriesFragment.java
@@ -99,7 +99,7 @@ public class CountriesFragment extends Fragment {
      */
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.search_menu, menu);
+        inflater.inflate(R.menu.search_refresh_menu, menu);
 
         MenuItem searchItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) searchItem.getActionView();
