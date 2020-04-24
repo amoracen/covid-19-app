b1cad59dfd60b25236484d602228c25e08b3153e
Álejandro Moracen <amoracen93@gmail.com>
2020-04-24 18:33:10 -0400

    add search bar

--- app/src/main/java/fau/amoracen/covid_19update/ui/homeActivity/news/NewsFragment.java
+++ app/src/main/java/fau/amoracen/covid_19update/ui/homeActivity/news/NewsFragment.java
@@ -1,5 +1,6 @@
 package fau.amoracen.covid_19update.ui.homeActivity.news;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,8 +9,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Toast;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,7 +23,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
-import java.util.Calendar;
+import java.util.Objects;
 
 import fau.amoracen.covid_19update.R;
 import fau.amoracen.covid_19update.data.NewsArticles;
@@ -30,11 +32,11 @@ import fau.amoracen.covid_19update.service.MySingleton;
 
 public class NewsFragment extends Fragment {
     private RecyclerView NewsRecyclerView;
-    private NewsAdapter adapter;
-    private long timeDataWasUpdated;
+    private ProgressBar progressBar;
+    private TextView messageTextView;
 
     /**
-     * Add a listener to the menu to refresh the data, and search the table
+     * Add a listener to the menu to search
      *
      * @param menu     a menu object
      * @param inflater inflate with the new menu
@@ -45,31 +47,21 @@ public class NewsFragment extends Fragment {
 
         MenuItem searchItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        //searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                return false;
+                if (query.isEmpty()) {
+                    return false;
+                }
+                makeRequest(query.trim());
+                hideKeyboard(getView());
+                return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                //adapter.getFilter().filter(newText);
                 return false;
-            }
-        });
-        MenuItem refreshItem = menu.findItem(R.id.action_refresh);
-        refreshItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                long currentTime = System.currentTimeMillis();
-                //600 Seconds or 10 minutes to milliseconds
-                if (currentTime >= (timeDataWasUpdated + 600 * 1000)) {
-                    makeRequest();
-                } else {
-                    Toast.makeText(getContext(), "News are up to Date", Toast.LENGTH_LONG).show();
-                }
-                return true;
             }
         });
         super.onCreateOptionsMenu(menu, inflater);
@@ -87,30 +79,30 @@ public class NewsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         NewsRecyclerView = view.findViewById(R.id.newsRecyclerView);
-        makeRequest();
+        progressBar = view.findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        messageTextView = view.findViewById(R.id.errorTextView);
+        makeRequest("COVID");
     }
 
     /**
      * Start the Request to the API
      */
-    private void makeRequest() {
-        //String url = "https://newsapi.org/v2/top-headlines?country=us&q=COVID&from=2020-04-16&sortBy=publishedAt&apiKey=" + getContext().getString(R.string.news_api_key) + "&pageSize=15&page=1";
-        String url = "https://newsapi.org/v2/everything?q=COVID&from=2020-04-16&sortBy=publishedAt&apiKey=" + getContext().getString(R.string.news_api_key) + "&pageSize=25&page=1&language=en";
-        //Get date
-        Calendar calendar = Calendar.getInstance();
-        timeDataWasUpdated = calendar.getTimeInMillis();
+    private void makeRequest(String topic) {
+        String url = "https://newsapi.org/v2/everything?q=" + topic + "&from=2020-04-16&sortBy=publishedAt&apiKey=" + getContext().getString(R.string.news_api_key) + "&pageSize=50&page=1&language=en";
         APIRequest request = new APIRequest<>(url, NewsArticles.class, new Response.Listener<NewsArticles>() {
             @Override
             public void onResponse(NewsArticles response) {
+                progressBar.setVisibility(View.GONE);
+                messageTextView.setText(getString(R.string.latest_news));
                 updateUI(response.getArticles());
-                /*TODO SAVE SQLite*/
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 error.printStackTrace();
-                /*TODO Handle Error*/
-                Toast.makeText(getContext(), "Search Failed", Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.GONE);
+                messageTextView.setText(getString(R.string.error_news));
             }
         });
         // Add a request to your RequestQueue.
@@ -124,9 +116,26 @@ public class NewsFragment extends Fragment {
      */
     private void updateUI(NewsArticles.Articles[] articles) {
         Log.i("Articles", String.valueOf(articles.length));
-        adapter = new NewsAdapter(articles);
+        NewsRecyclerView.removeAllViews();
+        NewsAdapter adapter = new NewsAdapter(articles);
         NewsRecyclerView.setAdapter(adapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
         NewsRecyclerView.setLayoutManager(linearLayoutManager);
+        NewsRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Hide Keyboard form View
+     *
+     * @param view current View
+     */
+    private void hideKeyboard(View view) {
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) Objects.requireNonNull(getContext()).getSystemService(Context.INPUT_METHOD_SERVICE);
+            assert inputMethodManager != null;
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
